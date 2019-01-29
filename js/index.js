7fee5bcd4eb4ef98db72d129d0c8051a431b8ab6
Jacob Angulo <jacobangulo@hotmail.com>
2019-01-29 15:52:09 -0800

    added event listeners to the top nav to change the colors when the mouse is over them, added event listeners to the bottom buttons using gsap and the code from this morning

--- js/index.js
+++ js/index.js
@@ -1,1 +1,45 @@
 // Your code goes here
+// `mouseover`
+// `keydown`
+// `wheel`
+// `drag / drop`
+// `load`
+// `focus`
+// `resize`
+// `scroll`
+// `select`
+// `dblclick`
+
+let aTagFunThymez = document.querySelectorAll('.nav-link');
+console.log(aTagFunThymez);
+
+aTagFunThymez.forEach(element => {
+    element.addEventListener('mouseover', event => {
+        element.style.color = 'green';
+    }),
+    element.addEventListener('mouseout', event => {
+        element.style.color = '#212529'
+    })
+});
+
+const workBtn = document.querySelectorAll('.btn');
+workBtn.forEach(element => {
+    element.addEventListener('mouseover', e => {
+        console.log(`Work button fired a click event!`);
+        // ALL about stopping events from bubbling
+        e.stopPropagation();
+        TweenMax.to(e.currentTarget, 1, {
+          width: 250,
+          ease:Bounce.easeOut
+        });
+    });
+    element.addEventListener('mouseout', e => {
+        e.stopPropagation();
+        TweenMax.to(e.currentTarget, 1, {
+            width:200,
+            ease: Bounce.easeOut
+        })
+    })
+})
+
+
